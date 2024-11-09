4f40303417c888c2466b00d94309082cd5f65e83
jiyoung <jiyoung_06@ewhain.net>
2024-11-09 11:23:59 +0900

    [Feat] Title(+Manual), Ending scene

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void ShowGameManual()
+    {
+        Debug.Log("manual");
+    }
+
+    public static void ExitGame()
+    {
+#if UNITY_EDITOR // develop
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public static void StartGame()
+    {
+        Debug.Log("start");
+        SceneManager.LoadScene("Play");
+    }
+
+    public static void ReturnToTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
