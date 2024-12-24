b904cbcbce7965dbc46ca36acddf866fed57d958
jiyoung <jiyoung_06@ewhain.net>
2024-12-24 17:43:37 +0900

    [Add] initial random nickname

--- Assets/Scripts/UI/Nickname.cs
+++ Assets/Scripts/UI/Nickname.cs
@@ -1,25 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Nickname : MonoBehaviour
 {
-    public InputField nicknameInput;
+    public TMP_InputField nicknameInput;
     public static string nickname = null;
+    [SerializeField]
+    private GameObject warningText;
 
     private void Awake()
     {
         if (nickname != null) gameObject.SetActive(false);
+        else
+        {
+            var randomNumber = $"{Random.Range(1, 1000)}".PadLeft(3, '0');
+            nicknameInput.text = $"익명{randomNumber}";
+        }
     }
 
     private void Update()
     {
+        if (nicknameInput.text.Length > 5)
+        {
+            nicknameInput.text = nicknameInput.text.Substring(0, 5);
+            warningText.SetActive(true);
+        }
         nickname = nicknameInput.text;
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ConfirmNickName();
+        }
     }
 
     //���콺
-    public async void InputName()
+    public async void ConfirmNickName()
     {
         if (nickname != null && nickname.Length > 0)
         {
